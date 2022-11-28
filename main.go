a93b12dc14c867834107586c31a02ec9d3b0ecc5
Mарат Исаков <valkyr1990@gmail.com>
2022-11-28 22:26:11 +0500

    my second commit

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	var a int
+	//c = 7
+	a = 8
+	//b = 91
+	//c = a * b
+	fmt.Println("Введите любое целое число:")
+	fmt.Scan(&a)
+	fmt.Println("Вы ввели число:")
+	fmt.Println(a)
 }
