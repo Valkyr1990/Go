9a20438076cd6004d2c48b67e492b82d29071ede
Mарат Исаков <valkyr1990@gmail.com>
2022-11-29 21:36:48 +0500

    AND_OR_NOT_TRUE_FALSE

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
-
 func main() {
-	var b bool = 4 > 5 || 6 > 8   // false
-	var c bool = 3 == 5 || 10 > 8 // true
-	fmt.Println(b, c)             // false
+	//var b bool = 4 > 5 || 6 > 8   // false
+	//var c bool = 3 == 5 || 10 > 8 // true
+	//fmt.Println(c, b)           // false
+
+	// Операция И
+	// Операция ИЛИ
+	// Операция НЕ
+
+	// ИЛИ
+	1 + 1 = 1
+	0 + 1 = 1
+	1 + 0 = 1
+	0 + 0 = 0
+
+	// И
+	1 + 1 = 1
+	1 + 0 = 0
+	0 + 1 = 0
+	0 + 0 = 0
+
+	// ИЛИ
+	true || true = true
+	false || true = true
+	true || false = true
+	false || false = false
+
+	// И
+	true && true = true
+	true && false = false
+	false && true = false
+	false && false = false
+
 }
