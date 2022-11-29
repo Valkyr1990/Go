30c6edeb552492eb3a748f71daa7fef2da54f9c9
Mарат Исаков <valkyr1990@gmail.com>
2022-11-29 21:20:41 +0500

    smth

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 8
-	var b int = 3
-	var c bool = a == b
-	fmt.Println(c) // false
+	var b bool = 4 > 5 || 6 > 8   // false
+	var c bool = 3 == 5 || 10 > 8 // true
+	fmt.Println(b, c)             // false
 }
