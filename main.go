0f11fbf64e2f4530c17e3d9206e66ab674916a77
Mарат Исаков <valkyr1990@gmail.com>
2022-12-03 13:27:51 +0500

    9

--- main.go
+++ main.go
@@ -3,19 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var number int
-	var next_digit int
-	var current_number int
-	var first_digit int
-	fmt.Scan(&number)
-	first_digit = number / 100
-	current_number = number % 10
-	number = number / 10
-	next_digit = number % 10
-	if first_digit == current_number == next_digit {
-		fmt.Print("YES")
+	var b, c, a int
+	for fmt.Scan(&b); b != 0; fmt.Scan(&b) {
+		if b > c {
+			a = 0
+			c = b
+		}
+		if b == c {
+			a += 1
+		}
+
 	}
-	else first_digit != current_number != next_digit{
-		fmt.Print("NO"),
-	}
+	fmt.Println(a)
 }
