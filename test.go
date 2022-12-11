5f2d397402f81cd98db18b9793d3b9dbd94095db
Mарат Исаков <valkyr1990@gmail.com>
2022-12-11 17:46:59 +0500

    13

--- test.go
+++ test.go
@@ -3,17 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var workArray [10]uint8
-	var a, q, w, e, r, t, y uint8
-	for i := 0; i < 10; i++ {
-		fmt.Scan(&a)
-		workArray[i] = a
+	var a, b, c int
+	fmt.Scan(&a)
+	for i := 0; i < a; i++ {
+		fmt.Scan(b)
+		if b == 0 {
+			c++
+		}
 	}
-	fmt.Scan(&q, &w, &e, &r, &t, &y)
-	workArray[q], workArray[w] = workArray[w], workArray[q]
-	workArray[e], workArray[r] = workArray[r], workArray[e]
-	workArray[t], workArray[y] = workArray[y], workArray[t]
-	for i := 0; i < len(workArray); i++ {
-		fmt.Print(workArray[i], " ")
-	}
+
 }
