d113cd84e5b42faabf9fbf49a914463c059780bb
Mарат Исаков <valkyr1990@gmail.com>
2022-12-13 11:51:55 +0500

    15

--- test.go
+++ test.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var x, q, c, a, w int
+	var x, a, result int
+	q := 1
 	fmt.Scan(&x)
+	fmt.Scan(&a)
 	for i := 10; i <= x*10; i *= 10 {
 		t1 := (x % i) / (i / 10)
-		q = q + t1
-		for q > 9 {
-			c = q / 10
-			a = q % 10
+		if t1 != a {
+			result = result + t1*q
+			q = q * 10
 		}
-		w = a + c
 	}
-	fmt.Print(w)
+	fmt.Print(result)
 }
